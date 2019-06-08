70719c48b5c885ad5f9d0aee8a895c3e3e55df86
aureopaquete <aureopaquete007@outlook.com>
2019-06-08 02:20:38 +0100

    Criada a classe BdNovaGibutaDietaOpenHelper (para abrir a base de dados)

---
+++ app/src/main/java/com/example/novagibutadieta/BdNovaGibutaDietaOpenHelper.java
@@ -0,0 +1,27 @@
+package com.example.novagibutadieta;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+
+public class BdNovaGibutaDietaOpenHelper extends SQLiteOpenHelper {
+
+    public static final String NOME_BASE_DADOS = "NovaGibutaDieta.db";
+    private static final int VERSAO_BASE_DADOS = 1;
+
+    public BdNovaGibutaDietaOpenHelper(@Nullable Context context) {
+        super(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
